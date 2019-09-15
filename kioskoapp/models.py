97a23ead9f320fff53735acccae50e0ecb086290
Knd9 <candela.fiume@gmail.com>
2019-09-15 14:37:14 -0300

    models and requirements

--- kioskoapp/models.py
+++ kioskoapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    codigo = models.CharField(max_length=100, unique=True, blank=True, null=False)
+    codigo = models.CharField(max_length=100, unique=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=5, decimal_places=2, null=True)
