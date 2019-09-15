d7c5982cbfe65c948a2e1e4ede1d396ba6892390
Knd9 <candela.fiume@gmail.com>
2019-09-15 16:11:10 -0300

    minimum validator

--- kioskoapp/models.py
+++ kioskoapp/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from django.db.models import DecimalField
+from django.core.validators import MinValueValidator
+
 # Create your models here.
 class Product(models.Model):
     codigo = models.CharField(max_length=100, unique=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    precio = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator('0')])
 
     def __str__(self):
         return self.nombre
