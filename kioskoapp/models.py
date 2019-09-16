6bc68a80687942dd47ce2dbb2c4c7b59943dd8fe
Knd9 <candela.fiume@gmail.com>
2019-09-15 21:30:42 -0300

    nothing change

--- kioskoapp/models.py
+++ kioskoapp/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     codigo = models.CharField(max_length=100, unique=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator('0')])
+    precio = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
 
     def __str__(self):
         return self.nombre
